ca4a597f9fc14bc6217c7f5a358f0e850df718f2
Chris Nuernberger <chris@techascent.com>
2019-06-05 16:50:13 -0600

    1.0-alpha10

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject techascent/tech.ml.dataset "1.0-alpha10-SNAPSHOT"
+(defproject techascent/tech.ml.dataset "1.0-alpha10"
   :description "Dataset and ETL pipeline for machine learning"
   :url "http://github.com/techascent/tech.ml.dataset"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1-beta2"]
                  [camel-snake-kebab "0.4.0"]
-                 [techascent/tech.datatype "4.0-alpha40"]
+                 [techascent/tech.datatype "4.0-alpha41"]
                  [tech.tablesaw/tablesaw-core "0.30.2"]
                  [com.github.haifengl/smile-core "1.5.2"]
                  [com.github.haifengl/smile-netlib "1.5.2"]]
