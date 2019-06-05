c1c3f2bf92da5e647bdc6c44b682fc78b8704c7d
Chris Nuernberger <chris@techascent.com>
2019-06-05 16:20:25 -0600

    large datatype library upgrade.

--- project.clj
+++ project.clj
@@ -5,12 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1-beta2"]
                  [camel-snake-kebab "0.4.0"]
-                 [techascent/tech.datatype "4.0-alpha28"]
+                 [techascent/tech.datatype "4.0-alpha40"]
                  [tech.tablesaw/tablesaw-core "0.30.2"]
                  [com.github.haifengl/smile-core "1.5.2"]
                  [com.github.haifengl/smile-netlib "1.5.2"]]
 
-  :profiles {:dev {:dependencies [[org.clojure/tools.logging "0.3.1"]
-                                  [ch.qos.logback/logback-classic "1.1.3"]]}}
+  :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.1.3"]]}}
 
   :test-selectors {:default (complement :disabled)})
