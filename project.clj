cc87782c8c5b60e3eb401ee407b85fe89a9fdcea
Chris Nuernberger <chris@techascent.com>
2019-06-11 14:59:58 -0600

    Moving project to deps now that it has stabilized.

--- project.clj
+++ project.clj
@@ -3,13 +3,7 @@
   :url "http://github.com/techascent/tech.ml.dataset"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1-beta2"]
-                 [camel-snake-kebab "0.4.0"]
-                 [techascent/tech.datatype "4.0-alpha41"]
-                 [tech.tablesaw/tablesaw-core "0.30.2"]
-                 [com.github.haifengl/smile-core "1.5.2"]
-                 [com.github.haifengl/smile-netlib "1.5.2"]]
-
-  :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.1.3"]]}}
-
-  :test-selectors {:default (complement :disabled)})
+  :plugins [[lein-tools-deps "0.4.1"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
+  :profiles {:uberjar {:aot :all}})
