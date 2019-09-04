6a4520a53f4e913c56fb21d3057e11fb42a937b4
Chris Nuernberger <chris@techascent.com>
2019-09-04 13:13:22 -0600

    snap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.ml.dataset "1.20"
+(defproject techascent/tech.ml.dataset "1.21-SNAPSHOT"
   :description "Dataset and ETL pipeline for machine learning"
   :url "http://github.com/techascent/tech.ml.dataset"
   :license {:name "Eclipse Public License"
