83ee9182f0080cd56a60f35552053fae26e16ebd
Chris Nuernberger <chris@techascent.com>
2020-04-21 15:07:48 -0600

    Fixes #34

---
+++ src/tech/ml/dataset/parse/name_values_seq.clj
@@ -0,0 +1,52 @@
+(ns tech.ml.dataset.parse.name-values-seq
+  (:require [tech.v2.datatype.protocols :as dtype-proto]
+            [tech.v2.datatype :as dtype]
+            [tech.v2.datatype.casting :as casting]
+            [tech.ml.dataset.parse.mapseq :as parse-mapseq]
+            [tech.ml.dataset.impl.dataset :as ds-impl]
+            [tech.ml.protocols.dataset :as ds-proto]
+            [tech.ml.dataset.column :as ds-col]))
+
+
+(defn name-values-seq->dataset
+  "Given a sequence of [name data-seq], produce a columns.  If data-seq is
+  of unknown (:object) datatype, the first item is checked. If it is a number,
+  then doubles are used.  If it is a string, then strings are used for the
+  column datatype.
+  All sequences must be the same length.
+  Returns a new dataset"
+  [name-values-seq & {:keys [dataset-name]
+                      :or {dataset-name "_unnamed"}}]
+  (let [sizes (->> (map (comp dtype/ecount second) name-values-seq)
+                   distinct)
+        _ (when-not (= 1 (count sizes))
+            (throw (ex-info (format "Different sized columns detected: %s" sizes) {})))
+        name-order (map first name-values-seq)
+        ;;Allow explicit missing/etc to be passed in.
+        map-data (filter (comp map? second) name-values-seq)
+        ;;fastpaths for primitive arrays - no need to scan the data.
+        known-container-data (filter (comp casting/numeric-type?
+                                           dtype/get-datatype
+                                           second)
+                                     name-values-seq)
+        half-dataset (->> (concat map-data known-container-data)
+                          (map (fn [[colname values-seq]]
+                                 (if (map? values-seq)
+                                   (ds-col/ensure-column values-seq)
+                                   (ds-col/new-column colname values-seq))))
+                          (ds-impl/new-dataset dataset-name))
+        colname-set (set (ds-proto/column-names half-dataset))
+        leftover (remove (comp colname-set first) name-values-seq)
+        n-data (count leftover)
+        colnames (map first leftover)
+        ;;Object columns mean we have to scan everything manually.
+        values (->> (map second leftover)
+                    (apply interleave)
+                    (partition n-data)
+                    (map #(zipmap colnames %)))
+        leftover-ds (parse-mapseq/mapseq->dataset values)]
+    (-> (ds-impl/new-dataset dataset-name
+                             {}
+                             (concat (ds-proto/columns half-dataset)
+                                     (ds-proto/columns leftover-ds)))
+        (ds-proto/select name-order :all))))
--- test/tech/ml/dataset/object_columns_test.clj
+++ test/tech/ml/dataset/object_columns_test.clj
@@ -1,7 +1,9 @@
 (ns tech.ml.dataset.object-columns-test
   (:require [clojure.test :refer :all]
             [tech.ml.dataset :as ds]
-            [tech.v2.datatype :as dtype]))
+            [tech.v2.datatype :as dtype]
+            [tech.v2.datatype.datetime :as dtype-dt]
+            [tech.v2.tensor :as dtt]))
 
 
 
@@ -13,3 +15,18 @@
            (dtype/get-datatype (src-ds :b))))
     (is (= (vec (repeat 10 {:a 1 :b 2}))
            (vec (dtype/->reader (src-ds :b)))))))
+
+
+
+(deftest involved-object-columns
+  (let [src-ds (ds/name-values-seq->dataset
+                {:dates (list "2000-01-01" "2000-02-01" "2000-03-01"
+                              "2000-04-01" "2000-05-01")
+                 :integers (range 5)
+                 :durations (repeat 5 (dtype-dt/duration))
+                 :doubles (map double (range 5))
+                 :tensors (repeat 5 (dtt/->tensor (partition 2 (range 4))))})]
+    (is (= #{:float64 :packed-local-date :int64 :object
+             :packed-duration}
+           (->> (map dtype/get-datatype src-ds)
+                set)))))
