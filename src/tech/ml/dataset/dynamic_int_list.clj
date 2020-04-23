73ff67fd52038c226d429e94e16516a2baac61fe
Chris Nuernberger <chris@techascent.com>
2020-04-23 14:26:04 -0600

    2.0-beta-25

--- src/tech/ml/dataset/dynamic_int_list.clj
+++ src/tech/ml/dataset/dynamic_int_list.clj
@@ -2,6 +2,7 @@
   "An int-list implementation that resizes its backing store as it is required to hold
   wider data."
   (:require [tech.v2.datatype.protocols :as dtype-proto]
+            [tech.v2.datatype.typecast :as typecast]
             [primitive-math :as pmath]
             [tech.v2.datatype.list]
             [tech.parallel.for :as parallel-for])
@@ -14,6 +15,7 @@
            [tech.v2.datatype IntReader IntWriter]))
 
 (set! *warn-on-reflection* true)
+(set! *unchecked-math* :warn-on-boxed)
 
 
 (defmacro byte-range?
@@ -36,7 +38,8 @@
                                (.add new-list#
                                      (pmath/short
                                       (.get ^ByteList ~'backing-store iter#))))
-                             new-list#))))
+                             new-list#))
+     (set! ~'reader nil)))
 
 
 (defmacro promote-int!
@@ -48,24 +51,28 @@
                                  (.add new-list#
                                        (pmath/int
                                         (.get ^ByteList ~'backing-store iter#))))
-                               new-list#)))
+                               new-list#))
+       (set! ~'reader nil))
      (when (instance? ShortList ~'backing-store)
        (set! ~'backing-store (let [new-list# (IntArrayList. )]
                                (dotimes [iter# (.size ~'backing-store)]
                                  (.add new-list#
                                        (pmath/int
                                         (.get ^ShortList ~'backing-store iter#))))
-                               new-list#)))))
+                               new-list#))
+       (set! ~'reader nil))))
 
 
 
-(deftype DynamicIntList [^:volatile-mutable ^List backing-store]
+(deftype DynamicIntList [^:unsynchronized-mutable ^List backing-store
+                         ^:unsynchronized-mutable ^IntReader reader]
   dtype-proto/PDatatype
   (get-datatype [item] :int32)
   dtype-proto/PCountable
   (ecount [item] (.size ^List backing-store))
   dtype-proto/PClone
-  (clone [item] (DynamicIntList. (dtype-proto/clone backing-store)))
+  (clone [item] (DynamicIntList. (dtype-proto/clone backing-store)
+                                 nil))
   dtype-proto/PToList
   (convertible-to-fastutil-list? [item] true)
   (->list-backing-store [item] backing-store)
@@ -106,15 +113,37 @@
          (promote-int!)
          (.add ^IntList backing-store idx value)))))
   (getInt [this idx]
-    (cond
-      (instance? ByteList backing-store)
-      (pmath/int (.getByte ^ByteList backing-store idx))
-      (instance? ShortList backing-store)
-      (pmath/int (.getShort ^ShortList backing-store idx))
-      (instance? IntList backing-store)
-      (pmath/int (.getInt ^IntList backing-store idx))
-      :else
-      (throw (Exception. "Unexpected backing store type."))))
+    (when-not reader
+      (let [new-reader
+            (cond
+              (instance? ByteList backing-store)
+              (let [^ByteList backing-store backing-store
+                    n-elems (long (.size backing-store))]
+                (if-let [{:keys [java-array offset length]}
+                         (dtype-proto/->sub-array backing-store)]
+                  (let [offset (pmath/int offset)
+                        ^bytes java-array java-array]
+                    (reify IntReader
+                      (lsize [rdr] n-elems)
+                      (read [rdr idx]
+                        (pmath/int (aget java-array (pmath/+ (pmath/int idx)
+                                                             offset))))))
+                  (reify IntReader
+                    (lsize [rdr] n-elems)
+                    (read [rdr idx]
+                      (pmath/int (.getByte backing-store (pmath/int idx)))))))
+              (instance? ShortList backing-store)
+              (let [^ShortList backing-store backing-store
+                    n-elems (long (.size backing-store))]
+                (reify IntReader
+                  (lsize [rdr] n-elems)
+                  (read [rdr idx]
+                    (pmath/int (.getShort backing-store (pmath/int idx))))))
+              (instance? IntList backing-store)
+              (typecast/datatype->reader :int32 backing-store)
+              :else (throw (Exception. "Programmer error")))]
+        (set! reader new-reader)))
+    (.read reader idx))
   (setInt [this idx value]
     (locking this
       (cond
@@ -141,7 +170,8 @@
           (promote-int!)
           (.set ^IntList backing-store idx value)))))
   (subList [this start-off end-off]
-    (DynamicIntList. (.subList backing-store start-off end-off)))
+    (DynamicIntList. (.subList backing-store start-off end-off)
+                     nil))
   IntReader
   (read [item idx] (.getInt item (int idx)))
   IntWriter
@@ -168,8 +198,9 @@
 (defn dynamic-int-list
   [num-or-item-seq]
   (if (number? num-or-item-seq)
-    (DynamicIntList. (ByteArrayList/wrap (byte-array (long num-or-item-seq))))
-    (let [retval (DynamicIntList. (ByteArrayList.))]
+    (DynamicIntList. (ByteArrayList/wrap (byte-array (long num-or-item-seq)))
+                     nil)
+    (let [retval (DynamicIntList. (ByteArrayList.) nil)]
       (parallel-for/doiter
        next-val
        num-or-item-seq
