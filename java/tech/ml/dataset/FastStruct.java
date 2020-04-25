0795bd2bcfa20f4003d778cbccd2bcc30bfdb4ce
Chris Nuernberger <chris@techascent.com>
2020-04-25 10:52:16 -0600

    Optimization - faststruct can use hashmaps, datatype has faster readers for arrays

--- java/tech/ml/dataset/FastStruct.java
+++ java/tech/ml/dataset/FastStruct.java
@@ -14,10 +14,12 @@ import clojure.lang.IMapEntry;
 import clojure.lang.ASeq;
 import clojure.lang.Util;
 import clojure.lang.RT;
+import primitive_math.Primitives;
 import java.io.Serializable;
 import java.util.NoSuchElementException;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -25,21 +27,39 @@ import java.util.Collections;
 
 
 public class FastStruct extends APersistentMap implements IObj{
-  public final IPersistentMap slots;
+  public final Map slots; //HashMaps are faster than persistent maps.
   public final List vals;
   public final IPersistentMap ext;
   public final IPersistentMap meta;
 
-  public FastStruct(IPersistentMap _meta, IPersistentMap _slots,
-		    List _vals, IPersistentMap _ext){
-	this.meta = _meta;
-	this.ext = _ext;
-	this.slots = _slots;
-	this.vals = _vals;
+  public FastStruct(IPersistentMap _meta, Map _slots,
+		    List _vals, IPersistentMap _ext) {
+    this.meta = _meta;
+    this.ext = _ext;
+    this.slots = _slots;
+    this.vals = _vals;
   }
-  public FastStruct(IPersistentMap _slots, List _vals){
+
+  public FastStruct(Map _slots, List _vals) {
     this( PersistentHashMap.EMPTY, _slots, _vals,
 	  PersistentHashMap.EMPTY);
+  }
+
+  public static FastStruct createFromColumnNames(List colnames, List vals) {
+    int nEntries = colnames.size();
+    HashMap slots = new HashMap(nEntries);
+    for (int idx = 0; idx < nEntries; ++idx ) {
+      slots.put(colnames.get(idx), idx);
+    }
+    return new FastStruct(Collections.unmodifiableMap(slots), vals);
+  }
+
+  public int columnIndex(Object key) throws Exception {
+    Object v = slots.get(key);
+    if (v != null) {
+      return RT.uncheckedIntCast(v);
+    }
+    throw new Exception("Key was not found");
   }
 
   public IObj withMeta(IPersistentMap _meta){
@@ -57,16 +77,16 @@ public class FastStruct extends APersistentMap implements IObj{
   }
 
   public IMapEntry entryAt(Object key){
-	Map.Entry e = slots.entryAt(key);
-	if(e != null)
-	  return MapEntry.create(e.getKey(), vals.get(RT.intCast(e.getValue())));
-	return ext.entryAt(key);
+    Object v = slots.get(key);
+    if(v != null)
+      return MapEntry.create(key, vals.get(RT.uncheckedIntCast(v)));
+    return ext.entryAt(key);
   }
 
   public IPersistentMap assoc(Object key, Object val){
-    Map.Entry e = slots.entryAt(key);
-    if(e != null) {
-      int i = RT.intCast( e.getValue() );
+    Object v = slots.get(key);
+    if(v != null) {
+      int i = RT.uncheckedIntCast( v );
       List newVals = new ArrayList(vals);
       newVals.set(i,val);
       return new FastStruct(meta, slots, Collections.unmodifiableList(newVals), ext);
@@ -75,17 +95,17 @@ public class FastStruct extends APersistentMap implements IObj{
   }
 
   public Object valAt(Object key){
-    Object i = slots.valAt(key);
+    Object i = slots.get(key);
     if(i != null) {
-      return vals.get(RT.intCast(i));
+      return vals.get(RT.uncheckedIntCast(i));
     }
     return ext.valAt(key);
   }
 
   public Object valAt(Object key, Object notFound){
-    Object i = slots.valAt(key);
+    Object i = slots.get(key);
     if(i != null) {
-      return vals.get(RT.intCast(i));
+      return vals.get(RT.uncheckedIntCast(i));
     }
     return ext.valAt(key, notFound);
   }
@@ -97,9 +117,11 @@ public class FastStruct extends APersistentMap implements IObj{
   }
 
   public IPersistentMap without(Object key) {
-    Map.Entry e = slots.entryAt(key);
-    if(e != null)
-      return new FastStruct(meta, slots.without(key), vals, ext);
+    if(slots.containsKey(key)) {
+      HashMap newSlots = new HashMap(slots);
+      newSlots.remove(key);
+      return new FastStruct(meta, Collections.unmodifiableMap(newSlots), vals, ext);
+    }
     IPersistentMap newExt = ext.without(key);
     if(newExt == ext)
       return this;
@@ -108,7 +130,7 @@ public class FastStruct extends APersistentMap implements IObj{
 
   public Iterator iterator(){
     return new Iterator(){
-      private Iterator ks = slots.iterator();
+      private Iterator ks = slots.entrySet().iterator();
       private Iterator extIter = ext == null ? null : ext.iterator();
       public boolean hasNext(){
 	return (ks != null && ks.hasNext() || (extIter != null && extIter.hasNext()));
@@ -116,9 +138,9 @@ public class FastStruct extends APersistentMap implements IObj{
 
       public Object next(){
 	if(ks != null) {
-	  IMapEntry data = (IMapEntry) ks.next();
+	  Map.Entry data = (Map.Entry) ks.next();
 	  ks = ks.hasNext() ? ks : null;
-	  int valIdx = RT.intCast(data.getValue());
+	  int valIdx = RT.uncheckedIntCast(data.getValue());
 	  return new MapEntry( data.getKey(), vals.get(valIdx));
 	}
 	else if(extIter != null && extIter.hasNext()) {
@@ -136,7 +158,7 @@ public class FastStruct extends APersistentMap implements IObj{
   }
 
   public int count(){
-    return vals.size() + RT.count(ext);
+    return slots.size() + RT.count(ext);
   }
 
   public ISeq seq(){
@@ -144,8 +166,8 @@ public class FastStruct extends APersistentMap implements IObj{
   }
 
   public IPersistentCollection empty(){
-    ArrayList newData = new ArrayList(slots.count());
-    for (int idx = 0; idx < slots.count(); ++idx)
+    ArrayList newData = new ArrayList(slots.size());
+    for (int idx = 0; idx < slots.size(); ++idx)
       newData.add(null);
     return new FastStruct(slots, newData);
   }
