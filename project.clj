968f612c07f0ee38abd6d43a97d3fe05d98b5d7f
Chris Nuernberger <chris@techascent.com>
2020-05-23 08:30:17 -0600

    Pointing to SQL bindings.

--- project.clj
+++ project.clj
@@ -6,6 +6,5 @@
   :plugins [[lein-tools-deps "0.4.5"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   :lein-tools-deps/config {:config-files [:install :user :project]}
-  :profiles {:dev {:lein-tools-deps/config {:resolve-aliases [:test]}}
-             :uberjar {:aot :all}}
+  :profiles {:dev {:lein-tools-deps/config {:resolve-aliases [:test]}}}
   :java-source-paths ["java"])
