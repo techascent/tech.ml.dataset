121c9f15c9c4bb081a8fe1ade2a509c9d2f7734a
Chris Nuernberger <chris@techascent.com>
2020-06-02 10:38:42 -0600

    moving toward better vectors

--- src/tech/libs/smile/data.clj
+++ src/tech/libs/smile/data.clj
@@ -2,6 +2,7 @@
   (:require [tech.v2.datatype :as dtype]
             [tech.v2.datatype.typecast :as typecast]
             [tech.v2.datatype.datetime :as dtype-dt]
+            [tech.v2.datatype.casting :as casting]
             [tech.ml.dataset :as ds]
             [tech.ml.dataset.column :as ds-col]
             [clojure.set :as set])
@@ -68,12 +69,77 @@
        (mapv column->field)
        (#(StructType. ^List %))))
 
+(defmacro datatype->smile-vec-type
+  "Datatype has to be a compile time constant"
+  [datatype]
+  (case (casting/safe-flatten datatype)
+    :boolean `BooleanVector
+    :int8 `ByteVector
+    :int16 `ShortVector
+    :int32 `IntegerVector
+    :int64 `LongVector
+    :float32 `FloatVector
+    :float64 `DoubleVector
+    :string `StringVector
+    :object `Vector))
+
+
+(defmacro implement-smile-vector
+  [datatype column]
+  `(let [field (column->field col)
+        col-rdr (dtype/->reader col)
+        ^RoaringBitmap missing (ds-col/missing col)]
+    (reify BaseVector
+      (name [v] (.name field))
+      (type [v] (.type field))
+      (measure [v] (.measure field))
+      (size [v] (count col))
+      ;;Data is not always backed by array
+      (array [v] (dtype/->array col))
+      (toDoubleArray [v] (dtype/make-container :java-array :float64 col))
+      (toIntArray [v] (dtype/make-container :java-array :int32 col))
+      (toStringArray [v] (dtype/make-container :java-array :string col))
+      (get [v ^int i]
+        (when-not (.contains missing i)
+          (col-rdr i)))
+      (^BaseVector get [v ^ints indexes]
+       (column->smile (ds-col/select col indexes)))
+      (getByte [v i]
+        (.read (typecast/datatype->reader :int8 col-rdr) i))
+      (getShort [v i]
+        (.read (typecast/datatype->reader :int16 col-rdr) i))
+      (getInt [v i]
+        (.read (typecast/datatype->reader :int32 col-rdr) i))
+      (getLong [v i]
+        (.read (typecast/datatype->reader :int64 col-rdr) i))
+      (getFloat [v i]
+        (.read (typecast/datatype->reader :float32 col-rdr) i))
+      (getDouble [v i]
+        (.read (typecast/datatype->reader :float64 col-rdr) i))
+      (stream [v]
+        (.parallelStream ^Collection col-rdr)))))
+
 
 (defn column->smile
   ^BaseVector [col]
+  (let [col-base-dtype (dtype/get-datatype col)
+        col-rdr (if (dtype-dt/packed-datatype? col-base-dtype)
+                  (dtype-dt/unpack col)
+                  (dtype/->reader col))
+        col-dtype (dtype/get-datatype col-rdr)]
+    (case (casting/safe-flatten col-dtype)
+      :boolean (implement-smile-vector :boolean col col-rdr)
+      :int8 (implement-smile-vector :int8 col col-rdr)
+      :int16 (implement-smile-vector :int16 col col-rdr)
+      :int32 (implement-smile-vector :int32 col col-rdr)
+      :int64 (implement-smile-vector :int64 col col-rdr)
+      :float32 (implement-smile-vector :float32 col col-rdr)
+      :float64 (implement-smile-vector :float64 col col-rdr)
+      ))
   (let [field (column->field col)
         col-rdr (dtype/->reader col)
         ^RoaringBitmap missing (ds-col/missing col)]
+
     (reify BaseVector
       (name [v] (.name field))
       (type [v] (.type field))
