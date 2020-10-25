1cda52f1ace16314352a17f0faeca789fc093e1b
Chris Nuernberger <chris@techascent.com>
2020-10-25 14:19:42 -0600

    Timing a couple more approaches to some things.

--- test/tech/v3/dataset/github_test.clj
+++ test/tech/v3/dataset/github_test.clj
@@ -12,7 +12,26 @@
                                                           :key-fn keyword})]
     (is (every? keyword? (ds/column-names ds)))
     (is (= [8 30] (dtype/shape ds)))))
+(do
+  (require '[tech.v3.datatype.functional :as dfn])
+  (require '[tech.v3.datatype.argops :as argops])
+  (require '[tech.v3.datatype.unary-pred :as un-pred])
+  (defonce flights (ds/->dataset "https://raw.githubusercontent.com/Rdatatable/data.table/master/vignettes/flights14.csv")))
 
-(require '[tech.v3.datatype.functional :as dfn])
-(defonce flights  (ds/->dataset "https://raw.githubusercontent.com/Rdatatable/data.table/master/vignettes/flights14.csv"))
+(time (-> (dfn/+ (flights "arr_delay")
+                 (flights "dep_delay"))
+          (dfn/< 0)
+          (un-pred/bool-reader->indexes)
+          (dtype/ecount)))
+
+;;Another way to get the same result is to use summation.  Booleans are
+;;interpreted very specifically below where false is 0 and 1 is true.
+;;Double summation is very fast.
+(time (-> (dfn/+ (flights "arr_delay")
+                 (flights "dep_delay"))
+          (dfn/< 0)
+          (dfn/sum)))
+
+
+
   )
