3906a3901cbdeb8b46a983f1da01e7b1cdd615a0
Chris Nuernberger <chris@techascent.com>
2020-10-27 16:14:28 -0600

    Most types, metadata, and attempted parallelized loading (failed).

--- src/tech/v3/libs/parquet.clj
+++ src/tech/v3/libs/parquet.clj
@@ -4,11 +4,13 @@
             [tech.v3.dataset.impl.column-base :as col-base]
             [tech.v3.dataset.impl.column :as col-impl]
             [tech.v3.datatype.bitmap :as bitmap]
+            [tech.v3.datatype.base :as dtype-base]
             [tech.v3.datatype.datetime :as dtype-dt]
             [clojure.tools.logging :as log])
   (:import [smile.io HadoopInput]
            [tech.v3.datatype PrimitiveList]
            [org.apache.parquet.hadoop ParquetFileReader]
+           [org.apache.parquet.hadoop.metadata BlockMetaData ColumnChunkMetaData]
            [org.apache.parquet.column.page PageReadStore]
            [org.apache.parquet.column.impl ColumnReadStoreImpl]
            [org.apache.parquet.column ColumnDescriptor ColumnReader]
@@ -16,7 +18,7 @@
            [org.apache.parquet.schema OriginalType
             PrimitiveType$PrimitiveTypeName]
            [java.nio ByteBuffer ByteOrder]
-           [java.time Instant]))
+           [java.time Instant LocalDate]))
 
 
 (comment
@@ -30,7 +32,7 @@
 
 
 (def ^:private test-file "test/data/parquet/userdata1.parquet")
-
+(def ^:private test-file-2 "../../perf-benchmark/performance-benchmark-data/part-00000-2211c4d6-c1a7-4bbb-9a27-302a2cb9f30f-c000.snappy.parquet")
 
 (declare primitive-converter)
 
@@ -47,11 +49,6 @@
   ^PrimitiveConverter []
   (proxy [PrimitiveConverter] []
     (asGroupConverter [] (group-converter))))
-
-
-(defmulti ^:private parse-column
-  (fn [col-rdr ^ColumnDescriptor col-def n-rows]
-    (.. col-def getPrimitiveType getPrimitiveTypeName)))
 
 
 (defn- static-parse-column
@@ -86,6 +83,16 @@
      (.addObject container (.. col-rdr getBinary getBytes)))))
 
 
+(defn- original-type-equals?
+  [^ColumnDescriptor col-def orig-type]
+  (= (.. col-def getPrimitiveType getOriginalType) orig-type))
+
+
+(defmulti ^:private parse-column
+  (fn [col-rdr ^ColumnDescriptor col-def n-rows]
+    (.. col-def getPrimitiveType getPrimitiveTypeName)))
+
+
 
 (defmethod parse-column :default
   [^ColumnReader col-rdr ^ColumnDescriptor col-def ^long n-rows]
@@ -94,7 +101,7 @@
 
 (defmethod parse-column PrimitiveType$PrimitiveTypeName/BINARY
   [^ColumnReader col-rdr ^ColumnDescriptor col-def ^long n-rows]
-  (if (= (.. col-def getPrimitiveType getOriginalType) OriginalType/UTF8)
+  (if (original-type-equals? col-def OriginalType/UTF8)
     (static-parse-column
      col-rdr col-def n-rows :string
      (fn [^PrimitiveList container ^ColumnReader col-rdr]
@@ -122,18 +129,37 @@
 
 (defmethod parse-column PrimitiveType$PrimitiveTypeName/INT32
   [^ColumnReader col-rdr ^ColumnDescriptor col-def ^long n-rows]
-  (static-parse-column
-   col-rdr col-def n-rows :int32
-   (fn [^PrimitiveList container ^ColumnReader col-rdr]
-     (.addLong container (.getInteger col-rdr)))))
+  (if (original-type-equals? col-def OriginalType/DATE)
+    (static-parse-column
+     col-rdr col-def n-rows :packed-local-date
+     (fn [^PrimitiveList container ^ColumnReader col-rdr]
+       (.addObject container (LocalDate/ofEpochDay (.getInteger col-rdr)))))
+    (static-parse-column
+     col-rdr col-def n-rows :int32
+     (fn [^PrimitiveList container ^ColumnReader col-rdr]
+       (.addLong container (.getInteger col-rdr))))))
 
 
 (defmethod parse-column PrimitiveType$PrimitiveTypeName/INT64
   [^ColumnReader col-rdr ^ColumnDescriptor col-def ^long n-rows]
-  (static-parse-column
-   col-rdr col-def n-rows :int64
-   (fn [^PrimitiveList container ^ColumnReader col-rdr]
-     (.addLong container (.getLong col-rdr)))))
+  (cond
+    (original-type-equals? col-def OriginalType/TIMESTAMP_MILLIS)
+    (static-parse-column
+     col-rdr col-def n-rows :packed-instant
+     (fn [^PrimitiveList container ^ColumnReader col-rdr]
+       (.addObject container (dtype-dt/milliseconds-since-epoch->instant
+                              (.getLong col-rdr)))))
+    (original-type-equals? col-def OriginalType/TIMESTAMP_MICROS)
+    (static-parse-column
+     col-rdr col-def n-rows :packed-instant
+     (fn [^PrimitiveList container ^ColumnReader col-rdr]
+       (.addObject container (dtype-dt/microseconds-since-epoch->instant
+                              (.getLong col-rdr)))))
+    :else
+    (static-parse-column
+     col-rdr col-def n-rows :int64
+     (fn [^PrimitiveList container ^ColumnReader col-rdr]
+       (.addLong container (.getLong col-rdr))))))
 
 
 (defmethod parse-column PrimitiveType$PrimitiveTypeName/DOUBLE
@@ -161,35 +187,53 @@
 
 
 (defn- page->ds
-  [^PageReadStore page ^ParquetFileReader reader]
+  [^PageReadStore page ^ParquetFileReader reader ^BlockMetaData block-metadata]
   (let [file-metadata (.getFileMetaData reader)
         schema (.getSchema file-metadata)
         col-read-store (ColumnReadStoreImpl. page (group-converter) schema
                                              (.getCreatedBy file-metadata))
         n-rows (.getRowCount page)]
-    (->> (.getColumns schema)
-         (pmap (fn [^ColumnDescriptor col-def]
-                 (let [col-name (.. col-def getPrimitiveType getName)]
-                   (try
-                     (parse-column (.getColumnReader col-read-store col-def) col-def n-rows)
-                     (catch Throwable e
-                       (log/warnf e "Failed to parse column %s: %s"
-                                  col-name (.toString col-def))
-                       nil)))))
+    (->> (map (fn [^ColumnDescriptor col-def ^ColumnChunkMetaData col-metadata]
+                (let [col-name (.. col-def getPrimitiveType getName)
+                      stats (.getStatistics col-metadata)]
+                  (try
+                    (let [reader (locking col-read-store
+                                   (.getColumnReader col-read-store col-def))
+                          retval (parse-column reader col-def n-rows)
+                          converter (condp = (dtype-base/elemwise-datatype retval)
+                                      :string
+                                      (fn [^Binary data] (String. (.getBytes data)))
+                                      :packed-local-date
+                                      (fn [^long data] (LocalDate/ofEpochDay data))
+                                      identity)]
+                      (vary-meta
+                       retval
+                       merge {:min (converter (.genericGetMin stats))
+                              :max (converter (.genericGetMax stats))
+                              :num-missing (.getNumNulls stats)}))
+                    (catch Throwable e
+                      (log/warnf e "Failed to parse column %s: %s"
+                                 col-name (.toString col-def))
+                      nil))))
+              (.getColumns schema) (.getColumns block-metadata))
          (remove nil?)
          (ds-impl/new-dataset))))
 
 
 (defn- read-next-dataset
-  [^ParquetFileReader reader]
+  [^ParquetFileReader reader block-metadata block-metadata-seq]
   (if-let [page (.readNextRowGroup reader)]
-    (cons (page->ds page reader)
-          (lazy-seq (read-next-dataset reader)))
+    (cons (page->ds page reader block-metadata)
+          (lazy-seq (read-next-dataset reader
+                                       (first block-metadata-seq)
+                                       (rest block-metadata-seq))))
     (.close reader)))
+
 
 (defn read-parquet-file
   ([^String path options]
-   (let [reader (ParquetFileReader/open (HadoopInput/file path))]
-     (read-next-dataset reader)))
+   (let [reader (ParquetFileReader/open (HadoopInput/file path))
+         blocks (.getRowGroups reader)]
+     (read-next-dataset reader (first blocks) (rest blocks))))
   ([^String path]
    (read-parquet-file path nil)))
