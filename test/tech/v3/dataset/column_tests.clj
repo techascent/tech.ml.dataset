f1ce3395e4945e1208ab8d4677034d87e8e7f370
Kimi MA <kimi.im@outlook.com>
2020-12-16 04:39:44 +0800

    update select-columns, drop-columns and remove-columns (#189)
    * update select-columns, drop-columns and remove-columns
    
    * fix typo in docstring
    
    Fixes #188

--- test/tech/v3/dataset/column_tests.clj
+++ test/tech/v3/dataset/column_tests.clj
@@ -2,6 +2,7 @@
   (:require [tech.v3.datatype :as dtype]
             [tech.v3.dataset :as ds]
             [tech.v3.datatype.functional :as dfn]
+            [tech.v3.dataset.column-filters :as cf]
             [clojure.test :refer [deftest is]]))
 
 
@@ -18,3 +19,23 @@
            (vec (dfn/finite? short-col))))
     (is (= 25
            (Math/round (dfn/mean short-col))))))
+
+(deftest select-columns-test
+  (let [DS (ds/->dataset {:A [1 2 3]
+                          :B [4 5 6]
+                          :C ["A" "B" "C"]})]
+    (is (= (ds/select-columns DS [:C])
+           (ds/select-columns DS cf/categorical)))
+    (is (= (ds/select-columns DS cf/numeric)
+           (ds/select-columns DS [:A :B])))))
+
+(deftest drop-columns-test
+  (let [DS (ds/->dataset {:A [1 2 3]
+                          :B [4 5 6]
+                          :C ["A" "B" "C"]})]
+    (is (= (ds/drop-columns DS cf/categorical)
+           (ds/remove-columns DS cf/categorical)
+           (ds/select-columns DS [:A :B])))
+    (is (= (ds/drop-columns DS cf/numeric)
+           (ds/remove-columns DS cf/numeric)
+           (ds/select-columns DS [:C])))))
