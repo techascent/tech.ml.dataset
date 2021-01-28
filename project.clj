d472fac041ee40bd599880246c4339e53fd1a4c2
Chris Nuernberger <chris@techascent.com>
2021-01-28 16:02:45 -0700

    5.00-beta-28

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject techascent/tech.ml.dataset "5.00-beta-28-SNAPSHOT"
+(defproject techascent/tech.ml.dataset "5.00-beta-28"
   :description "Clojure high performance data processing system"
   :url "http://github.com/techascent/tech.ml.dataset"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure              "1.10.2-alpha1"]
                  [camel-snake-kebab                "0.4.2"]
-                 [cnuernber/dtype-next             "6.00-beta-20"]
+                 [cnuernber/dtype-next             "6.00-beta-21"]
                  [techascent/tech.io               "4.03"
                   :exclusions [org.apache.commons/commons-compress]]
                  [com.univocity/univocity-parsers  "2.9.0"]
