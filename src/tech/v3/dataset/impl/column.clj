9eb083f870bf2301a25daa8a3895309f1263aafe
Ethan Miller <ethanzanemiller@gmail.com>
2021-02-22 15:53:44 -0800

    Save working draft of index-structure creation with extensibility
    Uses multimethods.

--- src/tech/v3/dataset/impl/column.clj
+++ src/tech/v3/dataset/impl/column.clj
@@ -138,45 +138,93 @@
                   (make-buffer ~'missing ~'data)))
            ~'cached-vector)))
 
-
-(defn make-index-structure
-  "Returns an index structure that used to optimize certain data operations."
-  [data missing]
-  (let [data-datatype (-> data (dtype/elemwise-datatype))
-        collect-row-nums (fn [memo [k v]]
+(defn build-idx-to-row-map [data]
+  (let [collect-row-nums (fn [memo [k v]]
                            (if-let [existing (get memo k)]
                              (assoc memo k (into existing v))
-                             (assoc memo k v)))
-        idx-map (->> data
-                     (map-indexed (fn [row-number elem]
-                                    [elem [row-number]]))
-                     (reduce collect-row-nums {}))]
-    (cond
-      (casting/numeric-type? data-datatype) 
-      (java.util.TreeMap. ^java.util.Map idx-map)
+                             (assoc memo k v)))]
+    (->> data
+         (map-indexed (fn [row-number elem]
+                        [elem [row-number]]))
+         (reduce collect-row-nums {}))))
 
-      :else
-      nil)))
+(defn datetime-datatype? [dtype]
+  (let [dtype (casting/un-alias-datatype dtype)]
+    ;; TODO: Use contains? instead perhaps
+    (boolean (tech.v3.datatype.datetime.base/datatypes dtype))))
+
+(defmulti make-index-structure
+  (fn [data missing]
+    (let [data-datatype (dtype/elemwise-datatype data)]
+      (cond
+        (casting/numeric-type? data-datatype)
+        :numeric
+
+        (datetime-datatype? data-datatype)
+        :datetime
+
+        :else
+        data-datatype))))
+
+(defmethod make-index-structure :numeric
+  [data missing]
+  (let [idx-map (build-idx-to-row-map data)]
+    (println {:dispatch-type :numeric :data data :idx-map idx-map})
+    (java.util.TreeMap. ^java.util.Map idx-map)))
+
+(defmethod make-index-structure :datetime
+  [data missing]
+  (let [idx-map (build-idx-to-row-map data)]
+    (println {:dispatch-type :datetime :data data :idx-map idx-map})
+    (java.util.TreeMap. ^java.util.Map idx-map)))
+
+ (defmethod make-index-structure :object
+  [data missing] nil)
 
 (comment
   (def cx (new-column "x"
                       (dtype/make-reader :float32 9 (rand))
                       nil
                       nil))
+  (index-structure cx)
 
-  (class (index-structure cx))
 
   (def cy (new-column "y"
                       (dtype/make-reader :string 9 "hi!")
                       nil
                       nil))
 
-  (class (index-structure cy)))
+  (class (index-structure cy))
+
+  (require '[tech.v3.datatype.datetime :as dtdt])
+
+  (def cz
+    (new-column
+     "z"
+     (dtype/make-reader :local-date
+                        10
+                        (dtdt/plus-temporal-amount (dtdt/local-date) idx :days))
+      ;; (dtype/make-reader :local-date 10 (dtdt/local-date))
+      nil
+      nil))
+
+  (-> cz
+      (dtype/elemwise-datatype)
+      ;; (casting/un-alias-datatype)
+      ;; (datetime-datatype?)
+      ;; (casting/numeric-type?)
+      )
+
+  (index-structure cz)
+
+  )
 
 
 (defprotocol PHasIndexStructure
   (index-structure [this]))
 
+(def protocol PCanCreateIndexStructure
+  (make-index-structure [this]))
 
 (deftype Column
     [^RoaringBitmap missing
