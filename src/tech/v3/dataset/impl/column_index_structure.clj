f8e0c5c36cdd47a7f1d326a6d83fdc15bc806c8c
Ethan Miller <ethanzanemiller@gmail.com>
2021-02-23 23:22:54 -0800

    Remove extra lines

--- src/tech/v3/dataset/impl/column_index_structure.clj
+++ src/tech/v3/dataset/impl/column_index_structure.clj
@@ -3,7 +3,6 @@
             [tech.v3.datatype.casting :as casting]
             [tech.v3.datatype.argops :refer [arggroup]]))
 
-
 (defn index-structure-kind [data]
   (let [data-datatype (dtype/elemwise-datatype data)]
     (cond
@@ -13,17 +12,14 @@
       :else
       data-datatype)))
 
-
 (defmulti make-index-structure
   "Returns an index structure based on the type of data in the column."
   (fn [data _] (index-structure-kind data)))
-
 
 (defmethod make-index-structure ::numeric
   [data missing]
   (let [idx-map (arggroup data)]
     (java.util.TreeMap. ^java.util.Map idx-map)))
-
 
 (defmethod make-index-structure :object
   [data missing] nil)
