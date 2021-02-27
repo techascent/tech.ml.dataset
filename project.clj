7ed7ca1ab9427822f0ca91f8434ebb423e599992
Chris Nuernberger <chris@techascent.com>
2021-02-27 09:14:13 -0700

    5.01

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.ml.dataset "5.01-SNAPSHOT"
+(defproject techascent/tech.ml.dataset "5.01"
   :description "Clojure high performance data processing system"
   :url "http://github.com/techascent/tech.ml.dataset"
   :license {:name "Eclipse Public License"
@@ -79,6 +79,7 @@
                                    tech.v3.dataset.join
                                    tech.v3.dataset.modelling
                                    tech.v3.dataset.column
+                                   tech.v3.dataset.clipboard
                                    tech.v3.dataset.neanderthal
                                    tech.v3.dataset.categorical
                                    tech.v3.dataset.reductions
---
+++ src/tech/v3/dataset/clipboard.clj
@@ -0,0 +1,56 @@
+(ns tech.v3.dataset.clipboard
+  "Optional namespace that copies a dataset to the clipboard for pasting into
+  applications such as excel or google sheets.
+
+  Reading defaults to 'csv' format while writing defaults to 'tsv' format."
+  (:require [tech.v3.dataset :as ds]
+            [tech.v3.datatype.errors :as errors])
+  (:import [java.io ByteArrayInputStream ByteArrayOutputStream]
+           [java.awt.datatransfer Clipboard DataFlavor StringSelection Transferable]
+           [java.nio.charset StandardCharsets]
+           [java.awt Toolkit]))
+
+
+(set! *warn-on-reflection* true)
+
+
+(defn clipboard
+  "Get the system clipboard."
+  ^Clipboard []
+  (.getSystemClipboard (Toolkit/getDefaultToolkit)))
+
+
+(defn clipboard->dataset
+  "Copy from the clipboard to a dataset.  Options are passed into ->dataset,
+  and `:file-type` is defaulted to `:csv`."
+  [& [{:keys [file-type]
+       :or {file-type :csv}
+       :as options}]]
+  (try
+    (-> (.getTransferData (.getContents (clipboard) nil)
+                          (DataFlavor/stringFlavor))
+        (str)
+        (.getBytes  StandardCharsets/UTF_8)
+        (ByteArrayInputStream.)
+        (ds/->dataset (assoc options :file-type file-type)))
+    (catch Exception e
+      (errors/throwf "Failed to copy dataset from the clipboard,
+This can happen with headless JDK installations:
+%s" e))))
+
+
+(defn dataset->clipboard
+  "Copy a dataset to the clipboard.  File type is defaulted to 'tsv' as this appears
+  to have  wider compatibility across various spreadsheet toolkits."
+  [x & [{:keys [file-type]
+         :or {file-type :tsv}}]]
+  (try
+    (let [os (ByteArrayOutputStream.)
+          _ (ds/write! x os {:file-type file-type})
+          selection (StringSelection. (String. (.toByteArray os)
+                                               StandardCharsets/UTF_8))]
+      (.setContents (clipboard) selection selection))
+    (catch Exception e
+      (errors/throwf "Failed to copy dataset to the clipboard,
+This can happen with headless JDK installations:
+%s" e))))
