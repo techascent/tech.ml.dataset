badf6ae326e71bed935d2e002876e0d13962e8e9
Carsten Behring <carsten.behring@gmail.com>
2021-03-15 17:04:39 +0100

    added doc string to lifted functions (#217)

--- src/tech/v3/dataset/metamorph.clj
+++ src/tech/v3/dataset/metamorph.clj
@@ -13,8 +13,9 @@
 
 (defmacro build-pipelined-function
   [f m]
-  (let [args (map (comp vec rest) (:arglists m))]
-    `(defn ~(symbol (name f))
+  (let [args (map (comp vec rest) (:arglists m))
+        doc-string (:doc m)]
+    `(defn ~(symbol (name f)) {:doc ~doc-string}
        ~@(for [arg args
                :let [narg (mapv #(if (map? %) 'options %) arg)
                      [a & r] (split-with (partial not= '&) narg)]]
