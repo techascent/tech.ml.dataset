1893518bb49c404d7e239ebb8ad89bf7816e1612
Ethan Miller <ethanzanemiller@gmail.com>
2021-04-05 16:59:07 -0700

    Add rough draft coverage for categorical index-structure

--- src/tech/v3/dataset/impl/column_index_structure.clj
+++ src/tech/v3/dataset/impl/column_index_structure.clj
@@ -23,8 +23,15 @@
 
 (defmulti make-index-structure
   "Returns an index structure based on the type of data in the column."
-  (fn [data] (-> data elemwise-datatype datatype->object-class)))
+  (fn [data]
+    (println {:datatype (elemwise-datatype data) :klass (-> data elemwise-datatype datatype->object-class)})
+    (-> data elemwise-datatype datatype->object-class)))
 
+
+(defmethod make-index-structure ::categorical
+  [data]
+  (let [idx-map (arggroup data)]
+    (java.util.LinkedHashMap. ^java.util.Map idx-map)))
 
 ;; When tech.datatype does not know what something is it describes it
 ;; as an object (see tech.v3.datatype.casting/elemwise-datatype). This
@@ -35,3 +42,16 @@
   (let [idx-map (arggroup data)]
     (TreeMap. ^java.util.Map idx-map)))
 
+
+;; Build a custom hierarchy to identify categorical types. The types
+;; identified here matches the set identified as categorical in
+;; the check in tech.v3.dataset.column/new-column that sets the `categorical?`
+;; metadata, see:
+;; https://github.com/techascent/tech.ml.dataset/blob/master/src/tech/v3/dataset/impl/column.clj#L41
+(derive java.lang.String ::categorical)
+(derive clojure.lang.Keyword ::categorical)
+(derive clojure.lang.Symbol ::categorical)
+(prefer-method make-index-structure ::categorical java.lang.Object)
+
+;; symbols not loaded into dtype-next's type system by default
+(tech.v3.datatype.casting/add-object-datatype! :symbol clojure.lang.Symbol true)
