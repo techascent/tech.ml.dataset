40f9ed0f76c313315bb7afb87dba72a920d40012
Chris Nuernberger <chris@techascent.com>
2021-04-08 15:05:45 -0600

    snap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.ml.dataset "5.18"
+(defproject techascent/tech.ml.dataset "5.19-SNAPSHOT"
   :description "Clojure high performance data processing system"
   :url "http://github.com/techascent/tech.ml.dataset"
   :license {:name "Eclipse Public License"
