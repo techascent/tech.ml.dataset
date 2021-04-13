10b570466801563c839bce5e7ba642ad4cceca1b
Ethan Miller <ethanzanemiller@gmail.com>
2021-04-12 21:31:33 -0700

    Add some test for column index

--- test/tech/v3/dataset/column_index_structure_tests.clj
+++ test/tech/v3/dataset/column_index_structure_tests.clj
@@ -1,39 +1,55 @@
 (ns tech.v3.dataset.column-index-structure-tests
-  (:import [java.util TreeMap])
-  (:require [tech.v3.datatype :refer [make-container]]
-            [tech.v3.dataset :as ds]
-            [tech.v3.dataset.column :refer [index-structure]]
+  (:import  [java.util TreeMap LinkedHashMap])
+  (:require [tech.v3.dataset :as ds]
+            [tech.v3.dataset.column :refer [new-column index-structure]]
             [tech.v3.dataset.impl.column-index-structure :refer [select-from-index] :as col-index]
-            [clojure.test :refer [deftest is]]))
-
-(deftest slice-from-index-test
-  (let [DS (ds/->dataset {:A [-1 9 4 10]})]
-    (is (= {9 [1], 10 [3]}
-           (-> (:A DS)
-               index-structure
-               (select-from-index ::col-index/slice {:from 5 :to 10})))))
-  ;; categorical
-  (let [DS (ds/->dataset {:keywords [:a :b :c]
-                          :strings  ["a" "b" "c"]
-                          :symbols  ['a 'b 'c]})]
-    (is (= {:a [0], :c [2]}
-           (-> (:keywords DS)
-               index-structure
-               (select-from-index ::col-index/pick [:a :c]))))
-    (is (= {"a" [0], "c" [2]}
-           (-> (:strings DS)
-               index-structure
-               (select-from-index ::col-index/pick ["a" "c"]))))
-    (is (= {'a [0], 'c [2]}
-           (-> (:symbols DS)
-               index-structure
-               (select-from-index ::col-index/pick ['a 'c]))))))
+            [clojure.test :refer [testing deftest is]]))
 
 
-(comment
-  (let [DS (ds/->dataset {:keywords [:a :b :c]})]
-    (-> (:keywords DS)
-        index-structure
-        (select-from-index ::col-index/pick [:a :c])))
+(testing "index-structure"
+  (let [DS (ds/->dataset {:continuous  [1 2 3]
+                          :categorical [:a :b :c]})]
+    (is (= TreeMap
+           (-> (:continuous DS)
+               index-structure
+               type)))
+    (is (= LinkedHashMap
+           (-> (:categorical DS)
+               index-structure
+               type)))))
 
-  )
+
+(testing "select-from-index"
+  (deftest test-with-continuous-data
+    (let [DS (ds/->dataset {:continuous [-1 4 9 10]})]
+      (is (= {9 [2]
+              10 [3]}
+             (-> (:continuous DS)
+                 index-structure
+                 (select-from-index ::col-index/slice {:from 5 :to 10}))))
+      (is (= {9 [2]}
+             (-> (:continuous DS)
+                 index-structure
+                 (select-from-index ::col-index/slice {:from 5 :from-inclusive? false
+                                                       :to 10  :to-inclusive? false}))))))
+
+
+  (deftest test-categorical-data
+    (let [DS (ds/->dataset {:keywords [:a :b :c]
+                            :strings  ["a" "b" "c"]
+                            :symbols  ['a 'b 'c]})]
+      (is (= {"a" [0]
+              "c" [2]}
+            (-> (:strings DS)
+                index-structure
+                (select-from-index ::col-index/pick ["a" "c"]))))
+      (is (= {'a [0]
+              'c [2]}
+            (-> (:keywords DS)
+                index-structure
+                (select-from-index ::col-index/pick [:a :c]))))
+      (is (= {'a [0]
+              'c [2]}
+            (-> (:symbols DS)
+                index-structure
+                (select-from-index ::col-index/pick ['a 'c])))))))
