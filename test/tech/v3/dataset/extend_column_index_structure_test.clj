ca50f90d7f9d33bfb3226dad62d87167ddb85667
Ethan Miller <ethanzanemiller@gmail.com>
2021-04-18 22:37:53 -0700

    Remove test stub

--- test/tech/v3/dataset/extend_column_index_structure_test.clj
+++
@@ -1,9 +0,0 @@
-(ns tech.v3.dataset.extend-column-index-structure-tests
-  (:require [tech.v3.dataset :as ds]
-            [tech.v3.dataset.column :refer [new-column index-structure]]
-            [tech.v3.dataset.impl.column-index-structure :refer [select-from-index] :as col-index]
-            [tech.v3.datatype :as dtype]
-            [tech.v3.datatype :as dtype-casting]
-            [clojure.test :refer [testing deftest is]]))
-
-;; TODO Possibly to add a test of the extenisibility?
