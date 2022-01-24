d7c05657c15cd3b70f4ba0d649e8227e7506c2b8
Chris Nuernberger <chris@techascent.com>
2022-01-24 09:44:48 -0700

    Start of demo.

--- java_test/java/jtest/TMDDemo.java
+++ java_test/java/jtest/TMDDemo.java
@@ -9,6 +9,11 @@ import static java.lang.System.out;
 
 public class TMDDemo {
   public static void main(String[] args) {
-    out.println("Awesome sauce");
+    //Make dataset can take a string, inputStream, a sequence of maps or a map of columns with
+    //the map of columns being the most efficient.
+    //Default file formats:
+    //csv, tsv, csv.gz, tsv.gz, (compressed, general, and surprisingly fast) .nippy
+    Map ds = makeDataset("https://github.com/techascent/tech.ml.dataset/raw/master/test/data/stocks.csv");
+    println(head(ds));
   }
 }
