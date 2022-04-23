db4201da1f8ff27a77d8e604e8eb4bf9e37837d1
Chris Nuernberger <chris@techascent.com>
2022-04-23 06:07:36 -0600

    Updating project.

--- ubertest/project.clj
+++ ubertest/project.clj
@@ -2,6 +2,8 @@
   :dependencies [[org.clojure/clojure "1.10.3" :scope "provided"]
                  [techascent/tech.ml.dataset "6.084"]]
   :profiles
+  ;; You really should just aot your main namespace and not everything.
+  ;; But I have reports from the field of oddness when someone aot's everything.
   {:uberjar {:aot :all
              :main ubertest.main
              :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
