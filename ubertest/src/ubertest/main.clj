0b1a3f621c950b5d85aa6262484b9668d35855d0
Chris Nuernberger <chris@techascent.com>
2022-10-03 07:02:48 -0600

    Simplest possible hang on shutdown.

--- ubertest/src/ubertest/main.clj
+++ ubertest/src/ubertest/main.clj
@@ -1,9 +1,14 @@
 (ns ubertest.main
   (:require [tech.v3.dataset :as ds]
-            [tech.v3.libs.arrow :as arrow])
+            ;;[tech.v3.libs.arrow :as arrow]
+            )
   (:gen-class))
 
 
 (defn -main
   [& args]
+  (let [ds (ds/->dataset {:a (range 10)
+                          :b (mapv double (range 10))})]
+    ;;(arrow/dataset->stream! ds "test.arrow")
+    )
   (println "exiting main"))
