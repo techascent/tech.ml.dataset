62eb876870ce3ac638e87083926ad3df199abbe7
Chris Nuernberger <chris@techascent.com>
2022-12-10 09:36:21 -0700

    Ensure single row datasets work - fix was in dtype.

--- neanderthal/tech/v3/dataset/neanderthal_test.clj
+++ neanderthal/tech/v3/dataset/neanderthal_test.clj
@@ -33,3 +33,34 @@
                     (map vector
                          (ds/columns test-ds)
                          (ds/columns res-ds))))))))
+
+
+(deftest single-row-test
+  (let [test-ds (ds/->dataset {:a [1]
+                               :b [6]
+                               :c [11]})]
+    (testing "Column major conversion"
+      (let [n-mat (ds-neanderthal/dataset->dense test-ds :column)
+            res-ds (ds-neanderthal/dense->dataset n-mat)]
+        (is (= 3 (ds/column-count res-ds)))
+        (is (every? #(dfn/equals (first %) (second %))
+                    (map vector
+                         (ds/columns test-ds)
+                         (ds/columns res-ds))))))
+    (testing "Row major conversion"
+      (let [n-mat (ds-neanderthal/dataset->dense test-ds :row)
+            res-ds (ds-neanderthal/dense->dataset n-mat)]
+        (is (= 3 (ds/column-count res-ds)))
+        (is (every? #(dfn/equals (first %) (second %))
+                    (map vector
+                         (ds/columns test-ds)
+                         (ds/columns res-ds))))))
+    (testing "Column major conversion - float32"
+      (let [n-mat (ds-neanderthal/dataset->dense test-ds :column :float32)
+            res-ds (ds-neanderthal/dense->dataset n-mat)]
+        (is (= 3 (ds/column-count res-ds)))
+        (is (every? #(dfn/equals (first %) (second %))
+                    (map vector
+                         (ds/columns test-ds)
+                         (ds/columns res-ds)))))))
+  )
